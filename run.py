11efe1148c6768959be41d84479a8250d5881f75
Charlotte Galpin <charlotte.galpin92@gmail.com>
2024-08-26 20:31:33 +0000

    created new display rules function which displays rules as print statements as better user experience

--- run.py
+++ run.py
@@ -16,17 +16,26 @@ SHEET = GSPREAD_CLIENT.open('hangman_words')
 
 words = SHEET.worksheet('words')
 
-rules = SHEET.worksheet('rules')
+
 
 #all_words = words.get_all_values()
 
 #display_rules = rules.get_all_values()
 
+"""
 def display_rules():
+    rules = SHEET.worksheet('rules')
     list_rules = rules.get_all_values()
     for i in list_rules:
         print(i)
-
+"""  
+def display_rules():
+    print("The Computer will select a Random Word and display a list of dashes to represent that word\n")
+    print("Your aim is to guess what the Random Word is by entering one letter at a time\n")
+    print("Enter any letter between 'a' and 'z' where indicated\n")
+    print("If the letter is correct it will appear in the correct space within the word\n")
+    print("If the letter is incorrect a section of the Hangman's Gallows will be added\n")
+    print("You will have 11 attempts to guess the correct word before the Hangman's Gallows are complete and you loose the game\n")
 
 
 HANGMAN = [
